567c5686b899b962efc744eb4560276317cc8852
Jonathan Giannuzzi <jonathan@giannuzzi.me>
2026-01-16 14:39:44 +0000

    CI/CD script

--- win32/msbuild.ps1
+++ win32/msbuild.ps1
@@ -40,19 +40,21 @@ if ($LASTEXITCODE -ne 0) {
 }
 # Now run only those tests with different limits
 
-# Tests needing increased number of file descriptors
-$msvcrtCheck = Add-Type -Member '[DllImport("msvcrt.dll")] public static extern int _getmaxstdio();' -Name "CheckValue" -PassThru
-$msvcrtSet = Add-Type -Member '[DllImport("msvcrt.dll")] public static extern int _setmaxstdio(int n);' -Name "SetValue" -PassThru
-$originalLimit = $msvcrtCheck::_getmaxstdio()
-$msvcrtSet::_setmaxstdio(2048)
-$env:TESTS_SKIP = "";
-$env:TESTS = $TESTS_WITH_INCREASED_NLIMIT;
-& ..\win32\outdir\$toolset\$platform\Release\tests.exe -l -Q
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "Consumer group test 0153 failed with exit code $LASTEXITCODE"
-    cd ..
-    exit $LASTEXITCODE
+# Tests needing increased number of file descriptors (only on x64)
+if ("x64" -eq $platform) {
+    $msvcrtCheck = Add-Type -Member '[DllImport("msvcrt.dll")] public static extern int _getmaxstdio();' -Name "CheckValue" -PassThru
+    $msvcrtSet = Add-Type -Member '[DllImport("msvcrt.dll")] public static extern int _setmaxstdio(int n);' -Name "SetValue" -PassThru
+    $originalLimit = $msvcrtCheck::_getmaxstdio()
+    $msvcrtSet::_setmaxstdio(2048)
+    $env:TESTS_SKIP = "";
+    $env:TESTS = $TESTS_WITH_INCREASED_NLIMIT;
+    & ..\win32\outdir\$toolset\$platform\Release\tests.exe -l -Q
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "Consumer group test 0153 failed with exit code $LASTEXITCODE"
+        cd ..
+        exit $LASTEXITCODE
+    }
+    $msvcrtSet::_setmaxstdio($originalLimit)
 }
-$msvcrtSet::_setmaxstdio($originalLimit)
 
 cd ..
